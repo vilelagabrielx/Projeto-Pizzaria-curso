35af2aa155716aec69d509f19bb25f3d14885815
neosyximplementacoes <133917835+neosyximplementacoes@users.noreply.github.com>
2024-02-24 00:23:28 -0300

    lavou ta novo

---
+++ BackEnd/src/middlewares/auth/isAuthenticatedByParam.ts
@@ -0,0 +1,36 @@
+import {Request,Response,NextFunction} from 'express'
+import {verify} from 'jsonwebtoken'
+
+
+interface Payload {
+    sub:string;
+}
+export function isAuthenticatedByParam(
+    request:Request,
+    response:Response,
+    next: NextFunction
+){
+ 
+    const authtoken = request.headers.authorization;
+
+    const id = request.params.idUser;
+
+    
+    if (!authtoken){
+        return response.status(401).end()      
+    }
+
+    const [, token] = authtoken.split(" ");
+
+    try {
+        const {sub} = verify(token,process.env.JWT_SECRET) as Payload
+        if (sub != id){
+            return response.status(401).json({error:"token invalid"})           
+        }
+        return next();
+    } catch (error) {
+        return response.status(401).end()
+    }
+
+ 
+}
--- BackEnd/src/routes/productRoutes.ts
+++ BackEnd/src/routes/productRoutes.ts
@@ -4,12 +4,13 @@ import { isAuthenticated } from "../middlewares/auth/isAuthenticated";
 import { validateEmptyParams } from '../middlewares/utils/validation';
 import { CreateProductController } from "../controllers/Products/CreateProductController";
 import { uploadToFolder } from "../middlewares/files/upload";
+import { isAuthenticatedByParam } from './../middlewares/auth/isAuthenticatedByParam';
 
 const productRoutes = Router();
 
 const upload =  uploadToFolder("products")
 
-productRoutes.post("/product",upload.single("banner"),validateEmptyParams(["name", "price", "description", "category_id"]),new CreateProductController().handle)
+productRoutes.post("/product/:idUser",isAuthenticatedByParam,upload.single("banner"),validateEmptyParams(["name", "price", "description", "category_id"]),new CreateProductController().handle)
 
 // categoryRoutes.put("/category",isAuthenticated, validateEmptyParams(['id','id_category','name']), new EditCategoryController().handle);
 
